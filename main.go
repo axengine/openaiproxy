476a06b6ee1b4dc8fa8f64ab1005e98fc1c09711
axengine <axengine@gmail.com>
2023-11-15 06:50:37 +0800

    use lts

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ import (
 var dst = flag.String("t", "https://api.openai.com", "targetURL,-t https://api.openai.com")
 var bind = flag.String("bind", ":8080", "bind host and port,like 127.0.0.1:8080")
 
+var keyFile = flag.String("keyfile", "", "")
+var certFile = flag.String("certfile", "", "")
+
 func main() {
 	flag.Parse()
 	// 创建目标 URL
@@ -36,5 +39,10 @@ func main() {
 
 	// 启动代理服务器
 	log.Println("bind", *bind, "proxy to ", (*targetURL).String())
-	log.Fatal(http.ListenAndServe(*bind, nil))
+	if *keyFile != "" && *certFile != "" {
+		log.Fatal(http.ListenAndServeTLS(*bind, *certFile, *keyFile, nil))
+	} else {
+		log.Fatal(http.ListenAndServe(*bind, nil))
+	}
+
 }
